852914f8baea311af67df083e8b67909dc0cc7fc
renner <80410025+renner0e@users.noreply.github.com>
2026-02-11 16:18:41 +0100

    feat: set new installer icon (#40)

--- iso_files/configure_iso_anaconda.sh
+++ iso_files/configure_iso_anaconda.sh
@@ -100,12 +100,12 @@ sed -i 's/ANACONDA_PRODUCTVERSION=.*/ANACONDA_PRODUCTVERSION=""/' /usr/{,s}bin/l
 
 # Add StartupWMClass so the running window inherits the icon
 desktop-file-edit \
-    --set-key=Icon --set-value=/usr/share/pixmaps/scope_installer.png \
+    --set-key=Icon --set-value=/usr/share/icons/hicolor/scalable/apps/dev.getaurora.installer.svg \
     --set-key=StartupWMClass --set-value=slitherer \
     /usr/share/applications/liveinst.desktop
 
 git clone https://github.com/get-aurora-dev/branding /tmp/branding
-cp -r /tmp/branding/iso_files/usr/share/pixmaps/* /usr/share/pixmaps
+cp -r /tmp/branding/iso_files/usr/* /usr/
 rm -rf /tmp/branding
 
 # Interactive Kickstart
